6cee307f4d4c5fde7d4d6c9bb1cb026a95d27d01
Anton Melser <anton@melser.org>
2025-01-24 09:15:36 +0800

    Minimum illustration of the generated col bug
    Signed-off-by: Anton Melser <anton.melser@outlook.com>

--- examples/todo-app/db/migrations/001-create-todos.sql
+++ examples/todo-app/db/migrations/001-create-todos.sql
@@ -1,6 +1,13 @@
 CREATE TABLE IF NOT EXISTS todos (
     id UUID PRIMARY KEY,
     title TEXT NOT NULL,
+    generated_bug SMALLINT GENERATED ALWAYS AS (
+        CASE
+            WHEN title = 'bug' THEN 0
+            WHEN title = 'another' THEN 1
+            ELSE -1
+        END
+    ) STORED,
     completed BOOLEAN NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
